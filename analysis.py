65c8877b3b3ec70ab90c3d1992c640110641d865
David Viggiano <91092410+dviggiano@users.noreply.github.com>
2022-10-15 00:47:41 -0500

    Update backend

--- analysis.py
+++ analysis.py
@@ -18,7 +18,7 @@ def analysis(username: str, password: str) -> str:
     spotify = Spotify(client_credentials_manager=SpotifyClientCredentials())
 
     # TODO run some analysis on listening history and playlists
-
-    uri = ""  # TODO combine analysis with user preferences from MongoDB and other factors to recommend a song
+    # TODO combine analysis with user preferences from MongoDB and other factors, such as time of year and popularity to recommend a song
+    uri = ""
 
     return uri
